2dd262e301c73ea6fcb52834ab131fe6a11cff82
BuczkoBalazs <buci85@gmail.com>
2022-01-19 17:58:51 +0100

    Getting familiar with data.

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,54 @@
+/* const data = [
+    "Gergely Kiss",
+    "Krisztián Bui",
+    "Buczkó Balázs",
+    "András Varga",
+    "Borbála Teréz Kovács",
+];
+ */
+
+const data = [
+    {
+        name: "Gergely Kiss",
+        favouriteFood: "csirke/rizs",
+    },
+    {
+        name: "Krisztián Bui",
+        favouriteFood: "banán",
+    },
+    {
+        name: "Buczkó Balázs",
+        favouriteFood: "kolbászos rájás pizza",
+    },
+    {
+        name: "András Varga",
+        favouriteFood: "brassói",
+    },
+    {
+        name: "Borbála Teréz Kovács",
+        favouriteFood: "avokádó",
+    },
+    {
+        name: "Benett Viszokai",
+        favouriteFood: "alma",
+    },
+    {
+        name: "Krisztián Pörneczi",
+        favouriteFood: "almás pite",
+    },
+]
 function loadEvent() {
     console.log("Page loaded :)")
 
-    const list = document.querySelectorAll("section")
+    const root = document.querySelector("#root")
 
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>text</p>
+    for (const frontendStudent of data){
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${frontendStudent.name}</h1>
+                <p>${frontendStudent.favouriteFood}</p>
+            </section>
         `)
-        //elem.querySelector("h1").outerHTML = ""
     }
 }
 window.addEventListener("load", loadEvent)
